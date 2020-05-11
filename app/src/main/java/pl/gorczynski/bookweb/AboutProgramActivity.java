4d86912a95ecbd1a4bd4f70cf8e04c6001126b70
mkgrnwat <mikolaj.gorczynski@student.wat.edu.pl>
2020-05-12 00:33:19 +0200

    Wykonanie 2.1

---
+++ app/src/main/java/pl/gorczynski/bookweb/AboutProgramActivity.java
@@ -0,0 +1,22 @@
+package pl.gorczynski.bookweb;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class AboutProgramActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_program);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("O programie");
+    }
+}
+
---
+++ app/src/main/java/pl/gorczynski/bookweb/MainActivity.java
@@ -0,0 +1,43 @@
+package pl.gorczynski.bookweb;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_about) {
+            Intent myintent = new Intent(MainActivity.this,
+                    AboutProgramActivity.class);
+            startActivity(myintent);
+        }
+        return false;
+
+
+    }
+}
